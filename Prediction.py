ff78dcfb51e57ef80de3f00bf43d3c92005a8ce6
jashan7305 <jashan070305@gmail.com>
2025-06-09 12:35:32 +0530

    condition added in Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -28,10 +28,19 @@ if model_name == "FruitClassification.h5":
         return label
     
 else:
-    
-    pass
 
-# trial_img = Image.open(data_dir + "\\test\\apple\\Image_1.jpg")
+    from torchvision import transforms
+
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+    def predict_image(img):
+        pass
+
+# trial_img = Image.open("train\\test\\apple\\Image_1.jpg")
 # predict_image(trial_img)
 
 
